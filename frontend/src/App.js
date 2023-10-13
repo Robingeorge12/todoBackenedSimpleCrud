f8d32c67b1fc8e5fb9278c1d3e3b8d74f9ca24d3
Robin George <robingsa.btch@gmail.com>
2023-10-13 10:22:08 +0530

    todofront

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AllRoute from './Route/AllRoute';
+
+function App() {
+  return (
+    <div className="App">
+  <Navbar />
+  <AllRoute />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Route/AllRoute.jsx
@@ -0,0 +1,17 @@
+import {Route,Routes} from  "react-router-dom"
+
+import React from 'react'
+
+function AllRoute() {
+  return (
+    <div>
+        <Routes>
+{/* <Route path="" element={< />} /> */}
+
+        </Routes>
+
+    </div>
+  )
+}
+
+export default AllRoute
